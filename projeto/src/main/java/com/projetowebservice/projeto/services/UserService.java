caa571b6d8d072a0a08524e07025e8b813462b27
Nicolas Santana <nicolassantana361@gmail.com>
2025-01-19 19:39:20 -0300

    Service layer, component registration

---
+++ projeto/src/main/java/com/projetowebservice/projeto/services/UserService.java
@@ -0,0 +1,30 @@
+package com.projetowebservice.projeto.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.projetowebservice.projeto.entities.User;
+import com.projetowebservice.projeto.repositories.UserRepository;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repository;
+
+    public List<User> findAll(){
+        return repository.findAll();
+    }
+
+    /**
+     * @param id
+     * @return
+     */
+    public User findById(Long id){
+        Optional<User> obj = repository.findById(id);
+        return obj.get();
+    }
+}
