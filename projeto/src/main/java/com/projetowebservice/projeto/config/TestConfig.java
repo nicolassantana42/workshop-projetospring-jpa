32615c7ebf28de78a86c6b35d854af2dd276b43d
Nicolas Santana <nicolassantana361@gmail.com>
2025-02-03 19:06:19 -0300

    Payment, one-to-one association

--- projeto/src/main/java/com/projetowebservice/projeto/config/TestConfig.java
+++ projeto/src/main/java/com/projetowebservice/projeto/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.projetowebservice.projeto.entities.Category;
 import com.projetowebservice.projeto.entities.Order;
 import com.projetowebservice.projeto.entities.OrderItem;
+import com.projetowebservice.projeto.entities.Payment;
 import com.projetowebservice.projeto.entities.Product;
 import com.projetowebservice.projeto.entities.User;
 import com.projetowebservice.projeto.entities.enums.OrderStatus;
@@ -81,5 +82,10 @@ public class TestConfig implements CommandLineRunner{
         OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice()); 
 
         orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));	
+
+        Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+        o1.setPayment(pay1);
+
+        orderRepository.save(o1);
     }
 }
