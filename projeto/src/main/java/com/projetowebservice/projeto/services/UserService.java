8ed686f32aec64ec5aaefda007ec7e2b76bb0279
Nicolas Santana <nicolassantana361@gmail.com>
2025-02-06 20:08:59 -0300

    Exception handling - update

--- projeto/src/main/java/com/projetowebservice/projeto/services/UserService.java
+++ projeto/src/main/java/com/projetowebservice/projeto/services/UserService.java
@@ -13,6 +13,8 @@ import com.projetowebservice.projeto.repositories.UserRepository;
 import com.projetowebservice.projeto.services.exceptions.DatabaseException;
 import com.projetowebservice.projeto.services.exceptions.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @Service
 public class UserService {
 
@@ -48,9 +50,13 @@ public class UserService {
     }
 
     public User update(Long id, User obj){
-        User entity = repository.getReferenceById(id);
-        updateData(entity, obj);
-                return repository.save(entity);
+        try {
+            User entity = repository.getReferenceById(id);
+            updateData(entity, obj);
+            return repository.save(entity);
+        } catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException(id);
+        }
             }
         
             private void updateData(User entity, User obj) {
